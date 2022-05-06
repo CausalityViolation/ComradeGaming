1b770ac443f6974d7f84b0e188d456c91ec96a0f
Causality <m.uddner@gmail.com>
2022-05-06 11:18:46 +0200

    fix security authority admin and user, update endpoints

--- src/main/java/com/example/comradegaming/auth/Security.java
+++ src/main/java/com/example/comradegaming/auth/Security.java
@@ -36,9 +36,18 @@ public class Security extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers("/login").permitAll();
-        http.authorizeRequests().antMatchers("/users/signup", "/refresh/**").permitAll();
-        http.authorizeRequests().antMatchers(GET, "/products/**", "/users/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN");
+        http.authorizeRequests().antMatchers("/login", "/users/signup", "/refresh/**").permitAll();
+        http.authorizeRequests().antMatchers(GET, "/products/**", "/users/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(GET, "/products/information/**", "/products", "/products/{\\\\d+}").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN");
+
+        http.authorizeRequests().antMatchers(GET, "/users/forsale/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(PATCH, "/users/update/**").hasAnyAuthority("ROLE_ADMIN");
+
+        //Buy och sell fixas i front-end där varje user bara kan köpa till sig själv
+        http.authorizeRequests().antMatchers(PATCH, "/users/buy/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(PATCH, "/users/forsale/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(PATCH, "/users/admin/**").hasAnyAuthority("ROLE_ADMIN");
+
         http.authorizeRequests().antMatchers(POST, "/products/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().antMatchers(PATCH, "/products/**").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().antMatchers(DELETE, "/products/**", "/users/**").hasAnyAuthority("ROLE_ADMIN");
